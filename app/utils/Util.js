b1b6209d2139e2b135d6430784598de4cc8fc1f7
burcakulug <burcak.ulug@feisystems.com>
2018-06-14 09:30:04 -0400

    Implement utility method to generate random string
    #20202:As a developer, I will implment Consent2Share as SMART App, so that it can be utilized in OCP SMART on FHIR Platform

--- app/utils/Util.js
+++ app/utils/Util.js
@@ -61,6 +61,16 @@ class Util {
     }
     return null;
   }
+
+  static randomString(length) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i += 1) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
 }
 
 export default Util;
