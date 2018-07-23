5a1ab1933689b4731ce856b1f25894fe254e6998
JiahaoLi <Jiahao.Li@feisystems.com>
2018-07-23 13:56:11 -0400

    Resolve to show careteams in consent card header
    #20764: C2S SoF App: Issues when used with secured FHIR server and workflows when fetching consents

--- app/components/ConsentCard/helpers.js
+++ app/components/ConsentCard/helpers.js
@@ -1,35 +1,44 @@
+import concat from 'lodash/concat';
+import union from 'lodash/union';
+
+const NEW_LINE_CHARACTER = '\n';
+
 export function flattenConsentData(consent) {
   return {
     ...consent,
-    fromActor: mapToConsentActors(consent.fromOrganizationActors, consent.fromPractitionerActors),
-    toActor: mapToConsentActors(consent.toOrganizationActors, consent.toPractitionerActors),
+    fromActor: mapToConsentActors(consent.fromOrganizationActors, consent.fromPractitionerActors, consent.fromCareTeams, consent.fromRelatedPersons),
+    toActor: mapToConsentActors(consent.toOrganizationActors, consent.toPractitionerActors, consent.toCareTeams, consent.toRelatedPersons),
     period: mapToPeriod(consent.period),
     purpose: mapToConsentPurpose(consent.purpose),
   };
 }
 
-const NEW_LINE_CHARACTER = '\n';
+function mapToConsentActors(organizationActors, practitionerActors, careTeams, relatedPersons) {
+  const organizationDisplays = organizationActors && organizationActors.length > 0 &&
+    concat(organizationActors
+      .map((actor) => `- ${actor.display}`)
+      .join(NEW_LINE_CHARACTER), NEW_LINE_CHARACTER);
+  const practitionerDisplays = practitionerActors && practitionerActors.length > 0 &&
+    concat(practitionerActors
+      .map((actor) => `- ${actor.display}`)
+      .join(NEW_LINE_CHARACTER), NEW_LINE_CHARACTER);
+  const careTeamDisplays = careTeams && careTeams.length > 0 &&
+    concat(careTeams
+      .map((actor) => `- ${actor.display}`)
+      .join(NEW_LINE_CHARACTER), NEW_LINE_CHARACTER);
+  const relatedPersonDisplays = relatedPersons && relatedPersons.length > 0 &&
+    concat(relatedPersons
+      .map((actor) => `- ${actor.display}`)
+      .join(NEW_LINE_CHARACTER), NEW_LINE_CHARACTER);
 
-function mapToConsentActors(organizationActors, practitionercActors) {
-  const actors = [];
-  actors.push(
-    (organizationActors && organizationActors.length > 0 && organizationActors
-    .map((actor) => `- ${actor.display}`)
-    .join(NEW_LINE_CHARACTER)));
-  actors.push(NEW_LINE_CHARACTER);
-  actors.push(
-    (practitionercActors && practitionercActors.length > 0 && practitionercActors
-    .map((actor) => `- ${actor.display}`)
-    .join(NEW_LINE_CHARACTER))
-  );
-  return actors;
+  return union(organizationDisplays, practitionerDisplays, careTeamDisplays, relatedPersonDisplays);
 }
 
 function mapToConsentPurpose(purposes) {
   return (
     (purposes && purposes.length > 0 && purposes
-    .map((purpose) => `- ${purpose.display}`)
-    .join(NEW_LINE_CHARACTER)));
+      .map((purpose) => `- ${purpose.display}`)
+      .join(NEW_LINE_CHARACTER)));
 }
 
 function mapToPeriod(period) {
